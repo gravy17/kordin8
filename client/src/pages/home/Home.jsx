9f7629bdb5bce5f60f21645e8021e02ad3dd9326
Akindele <akindeleapex@gmail.com>
2022-09-06 02:44:34 +0100

    front-end home updated

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -6,7 +6,55 @@ const Home = () => {
     <div className="home">
       <div className="homeContainer">
         <Navbar />
-        Home
+        <div className="homeContent">
+        <div className="images">
+            <div className="homeText">
+              <h1>Find your Luggages </h1>
+              <p>Ship, manage, track, deliver.</p>
+            </div>
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="homeSearch">
+            <input type="text" placeholder="TRACKING ID" /><button>Track</button>
+          </div>
+          </div>
+            <div className="page-below">
+              <div className="page-below-image"></div>
+              <div className="page-below-section">
+                <h2>Manage your shipment and returns</h2>
+                <div className="page-below-section-text">
+                  <div className="box">
+                  <img className="one" src="https://img.icons8.com/external-flaticons-flat-flat-icons/64/000000/external-notifications-automation-technology-flaticons-flat-flat-icons.png" alt="box" />
+                    <h3>Notifications</h3>
+                  </div>
+                  <div className="box">
+                  <img className="two" src="https://img.icons8.com/external-soft-fill-juicy-fish/60/000000/external-hour-contact-us-soft-fill-soft-fill-juicy-fish.png" alt="box" />
+                    <h3>24/7 Support</h3>
+                  </div>
+                  <div className="box">
+                  <img className="three" src="https://img.icons8.com/external-icongeek26-glyph-icongeek26/64/000000/external-lorry-transportation-icongeek26-glyph-icongeek26-1.png" alt="box" />
+                    <h3>Delivery</h3>
+                  </div>
+                  
+                  <div className="box">
+                  <img className="four" src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/000000/external-notifications-automation-technology-flaticons-lineal-color-flat-icons.png" alt="box" />
+                    <h3>Get Alerted</h3>
+                  </div>             
+     
+                </div>
+                
+              </div>
+            </div>
+           
+            <footer class="footer">
+                
+               
+              </footer>
+            
+            
+            
+        </div>
       </div>
     </div>
   );
--- client/src/pages/track/Track.jsx
+++ client/src/pages/track/Track.jsx
@@ -2,11 +2,19 @@ import "./track.scss"
 import Navbar from "../../components/navbar/Navbar"
 
 const Track = () => {
-  return (
+  
+      return (
     <div className="track">
       <div className="trackContainer">
         <Navbar />
-        Track
+        <div className="trackContent">
+            <div className="trackText">
+              <h2>Track your Luggages </h2>
+              <p>Ship, manage, track, deliver.</p>
+            </div>
+          </div>
+          <hr />
+          <h4>Your result is loading....</h4>
       </div>
     </div>
   )
