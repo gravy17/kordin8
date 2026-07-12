a49800c03b9ec47cbdba7120fa7908c2773539f3
Tofi Agbaje <tofarati.agbaje@gmail.com>
2026-07-12 04:30:22 +0100

    Deploy fix for FE

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,1 @@
-//export const SERVER_URL='http://localhost:3001';
-export const SERVER_URL='https://kordin8-lbnwolzz4-tofaratis-projects.vercel.app';
+export const SERVER_URL='https://kordin8-api.vercel.app';
