de6ebe4d3f60ca4edf939cec6d4e17dfca7181ff
Tofi Agbaje <tofarati.agbaje@gmail.com>
2026-07-12 03:49:18 +0100

    Fixing Deployment (bye Heroku, Hi Vercel)

--- server/seeders/20220831210221-super-user.js
+++ server/seeders/20220831210221-super-user.js
@@ -2,29 +2,40 @@
 require("dotenv").config();
 const uuidv4 = require("uuid").v4;
 const { hashSync } = require("bcryptjs");
-const id = uuidv4();
-const hash = hashSync(process.env.SUPERADMIN_PASSWORD, 8);
 const email = process.env.SUPERADMIN_EMAIL;
-const date = new Date();
 
 module.exports = {
   async up(queryInterface, Sequelize) {
+    const existing = await queryInterface.rawSelect(
+      "Admins",
+      { where: { email }, limit: 1 },
+      ["id"]
+    );
+
+    if (existing) {
+      return;
+    }
+
+    const id = require("uuid").v4();
+    const hash = require("bcryptjs").hashSync(process.env.SUPERADMIN_PASSWORD, 8);
+    const date = new Date();
+
     await queryInterface.bulkInsert("Admins", [
       {
-        id: id,
-        firstName: "Superadmin",
-        lastName: "Kordin8",
-        email: email,
+        id,
+        firstName: "Super",
+        lastName: "Admin",
+        email,
         phone: "01234567890",
         password: hash,
         address: "7 Asajon Way Sangotedo",
         createdAt: date,
-        updatedAt: date
-      }
+        updatedAt: date,
+      },
     ]);
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete("Admins", null, {});
-  }
+    await queryInterface.bulkDelete("Admins", { email }, {});
+  },
 };
